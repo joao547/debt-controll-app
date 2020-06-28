5652ddfaf05d1c673e181e1a5cdc9c8e246f7547
joao.victor01 <joao.victor@usto.re>
2020-06-28 00:04:46 -0300

    Criando sistema de rotas

--- src/main/App.js
+++ src/main/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
 
-import Login from './views/login';
-import CadastroUsuario from './views/cadastroUsuario';
+import Rotas from './rotas';
 
 import 'bootswatch/dist/flatly/bootstrap.css';
-import './custom.css';
+import '../custom.css';
 
 
 class App extends React.Component {
   render(){
     return(
-      <CadastroUsuario />
+      <div className="container">
+        <Rotas />
+      </div>
     )
   }
 }
